9ed154910d8424add5fa02790acea830c20bec30
cemozyaniz <cozyaniz@icloud.com>
2023-01-31 01:00:53 +0300

    all changes done

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskInputField from './components/TaskInputField';
+import TaskItem from './components/TaskItem';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const addTask = (task) => {
+    let myHeaders = {
+      "Content-Type" : "application/json",
+    };
+    if (task == null) return;
+    
+    let raw = JSON.stringify({
+      "name": task,
+      "description": "description 2"
+    });
+    
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("http://5.180.49.10:5001/api/todo", requestOptions)
+      .then(response => response.text())
+      .then(result => fetchData())
+      .catch(error => console.log('error', error));
+    Keyboard.dismiss()
+  }
+
+  const deleteTask = (deleteIndex) => {
+    var requestOptions = {
+      method: 'DELETE',
+      redirect: 'follow'
+    };
+    
+    fetch(`http://5.180.49.10:5001/api/todo/${deleteIndex}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    fetchData();
+  }
+
+  const fetchData = async () => {
+    const response = await fetch('http://5.180.49.10:5001/api/todo/?page=1');
+    const data = await response.json();
+    setData(data.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        data.map((item, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={item.name} deleteTask={() => deleteTask(item._id)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -13,8 +78,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#CE7777',
   },
+  heading: {
+    color: '#2B3A55',
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: 70,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
---
+++ components/TaskItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default TaskItem = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.indexContainer}>
+                <Text style={styles.index}>{props.index}</Text>
+            </View>
+            <View style={styles.taskContainer}>
+                <Text style={styles.task}>{props.task}</Text>
+                <TouchableOpacity onPress={() => props.deleteTask()}>
+                    <MaterialIcons style={styles.delete} name="delete" size={18} color='#fff' />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+    },
+    indexContainer: {
+        backgroundColor: '#2B3A55',
+        borderRadius: 12,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+    },
+    index: {
+        color: '#E8C4C4',
+        fontSize: 20,
+    },
+    taskContainer: {
+        backgroundColor: '#2B3A55',
+        borderRadius: 12,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        minHeight: 50,
+    },
+    task: {
+        color: '#E8C4C4',
+        width: '90%',
+        fontSize: 16,
+    },
+    delete: {
+        marginLeft: 10,
+    },
+});
